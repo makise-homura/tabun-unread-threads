4312968fd34a1bc7497f2523f7397ceb6ab4fb30
makise-homura <akemi_homura@kurisa.ch>
2023-01-18 22:14:41 +0300

    Add userscript description

--- unread.user.js
+++ unread.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         Непрочитанные посты на Табуне
-// @version      0.1.0
+// @version      0.1.1
+// @description  Добавляет на страницу публикаций залогиненного пользователя вкладу для просмотра постов с новыми комментариями
 // @author       makise_homura
 // @match        https://tabun.everypony.ru/*
 // @match        https://tabun.everypony.info/*
